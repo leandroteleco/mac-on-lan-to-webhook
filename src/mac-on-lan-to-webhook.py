901a5b09a4e8153514f71bf8e45f0f079682760d
Leandroteleco <leandropavonserrano@gmail.com>
2021-03-24 02:34:56 +0100

    rewriting sniff() function and checked the trigger_webhook() run with IFTTT

--- src/mac-on-lan-to-webhook.py
+++ src/mac-on-lan-to-webhook.py
@@ -10,7 +10,7 @@ import binascii
 
 # DEVICE 1
 DEVICE_1_MAC = os.environ.get('DEVICE_1_MAC')
-DEVICE_1_NAME = os.environ.get('DEVICE_1_NAME')
+DEVICE_1_NAME = os.environ.get('DEVICE_2_NAME')
 DEVICE_1_WEBHOOK = os.environ.get('DEVICE_1_WEBHOOK')
 
 # DEVICE 2
@@ -25,14 +25,17 @@ macs = {
 
 def trigger_webhook (my_webhook_url):
     url = my_webhook_url
-    payload = {"Payload" : "Payload"}
+    payload = "Payload=Payload"
     headers = {'Accept': 'application/json', 'Authorization': "Bearer " + "Bearer Token", 'Content-Type': 'application/x-www-form-urlencoded'}
     response = requests.request("GET", url, headers=headers, data=payload)
+    print("Response code: {}".format(response.status_code))
     if response.status_code != 200:
         print("ERROR: The error code is: {}".format(response.status_code))
-        print("Response: " + str(response.json()))
+        print("Response: " + response.text)
+        #print (json.dumps(response.json(), indent=4, sort_keys=True))
     else:
-        print("Response: " + str(response.json()))
+        print("Response: " + response.text)
+        #print (json.dumps(response.json(), indent=4, sort_keys=True))
     return response
 
 # Unpack ethernet frame
@@ -49,73 +52,35 @@ def get_mac_addr(bytes_addr):
 def sniff():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3)) # Just works on GNU/Linux!
     arp_code_protocol = 2054 # ARP Ethertype (0x0806) https://en.wikipedia.org/wiki/EtherType
+
     while True:
         raw_data, addr = conn.recvfrom(65536)
         my_packet = conn.recvfrom(2048)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
-        print('Ethernet Frame: ' + 'Destination: {}, Source: {}, Protocol: {} / {}'.format( dest_mac, src_mac, eth_proto, socket.ntohs(eth_proto)))
+        #print('Ethernet Frame: ' + 'Destination: {}, Source: {}, Protocol: {} / {}'.format( dest_mac, src_mac, eth_proto, socket.ntohs(eth_proto)))
+
         if socket.ntohs(eth_proto) != arp_code_protocol:
-            #print('my_ethertype: {}'.format(arp_code_protocol)) 
             continue
+
         # read out data 
         my_arp_header = my_packet[0][14:42]
         my_arp_detailed = struct.unpack("2s2s1s1s2s6s4s6s4s", my_arp_header)
-        my_source_mac = binascii.hexlify(my_arp_detailed[5])
+        my_source_mac = binascii.hexlify(my_arp_detailed[5]).decode('ascii').upper()
         my_source_ip = socket.inet_ntoa(my_arp_detailed[6])
         my_dest_ip = socket.inet_ntoa(my_arp_detailed[8])
-        print ("Unknown MAC " + get_mac_addr(my_source_mac) + " from IP " + my_source_ip)
+
         if my_source_mac in macs:
-            #print "ARP from " + macs[source_mac] + " with IP " + source_ip
+
             if macs[my_source_mac] == DEVICE_1_NAME:
+                print ("Device " + DEVICE_1_NAME + " with MAC " + my_source_mac + " and with IP " + my_source_ip + " is in the network.")
                 trigger_webhook(DEVICE_1_WEBHOOK)
+                
             if macs[my_source_mac] == DEVICE_2_NAME:
+                print ("Device " + DEVICE_2_NAME + " with MAC " + my_source_mac + " and with IP " + my_source_ip + " is in the network.")
                 trigger_webhook(DEVICE_2_WEBHOOK)
+                
         else:
-            print ("Unknown MAC " + get_mac_addr(my_source_mac) + " from IP " + my_source_ip)
-
-def test():
-    print("Starting the program: ")
-    try:
-        #print('Going to create socket...')
-        my_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003)) # Just works on GNU/Linux!
-        #print('... socket created! :)')
-    except socket.error as msg:
-        print('Socket could not be created -> Error code {}, Message: {}'.format(msg[0],msg[1]))
-
-    try:
-        while True:
-            #print('Going to create packet...')
-            my_packet = my_socket.recvfrom(2048)
-            #print('... packet created! :)')
-            my_ethernet_header = my_packet[0][0:14]
-            my_ethernet_detailed = struct.unpack("!6s6s2s", my_ethernet_header)
-            # skip non-ARP packets
-            my_ethertype = my_ethernet_detailed[2]
-            
-            print('Ethernet Frame: ' + 'Destination: {}, Source: {}, Protocol: {} '.format( my_ethernet_detailed[0], my_ethernet_detailed[1], my_ethertype))
-            arp_code_protocol = 'b\'\x08\x06\''
-            if str(my_ethertype) != arp_code_protocol:
-                print('my_ethertype: {}'.format(arp_code_protocol)) 
-                continue
-            # read out data 
-            my_arp_header = my_packet[0][14:42]
-            my_arp_detailed = struct.unpack("2s2s1s1s2s6s4s6s4s", my_arp_header)
-            my_source_mac = binascii.hexlify(my_arp_detailed[5])
-            my_source_ip = socket.inet_ntoa(my_arp_detailed[6])
-            my_dest_ip = socket.inet_ntoa(arp_detailed[8])
-            print ("Unknown MAC " + my_source_mac + " from IP " + my_source_ip)
-            if my_source_mac in macs:
-                #print "ARP from " + macs[source_mac] + " with IP " + source_ip
-                if macs[my_source_mac] == DEVICE_1_NAME:
-                    trigger_webhook(DEVICE_1_WEBHOOK)
-                if macs[my_source_mac] == DEVICE_2_NAME:
-                    trigger_webhook(DEVICE_2_WEBHOOK)
-            else:
-                print ("Unknown MAC " + my_source_mac + " from IP " + my_source_ip)
-   
-    except KeyboardInterrupt:
-        print("Press Ctrl-C to terminate while statement")
-        pass
+            print ("Unknown MAC " + my_source_mac + " with IP " + my_source_ip + " is in the network.")
 
 def main():
     sniff()
